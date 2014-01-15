03e03b10a58ac25978d81d8d4a29ad69b91a9cd2
Meghann Plunkett <plunkett.meghann@gmail.com>
2014-01-15 15:58:52 -0500

    added to config file for heroku

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
     puts "params:"
     puts request.env
+    ##can grab things out of this env hash###
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
